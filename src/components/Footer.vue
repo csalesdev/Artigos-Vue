385efd29e5b0da7a6f1029df9073b5add04124a6
Your Nam <ragzitos@gmail.com>
2024-12-12 20:45:30 -0300

    Criação do Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,61 @@
+<script setup>
+import LogoFooter from "../assets/logo-footer.png";
+</script>
+
+<template>
+  <footer>
+    <nav>
+      <ul>
+        <li><a href="#Contatos">Meus Contatos</a></li>
+        <li><a href="#LinkedIn">LinkedIn</a></li>
+        <li><a href="#GitHub">GitHub</a></li>
+      </ul>
+    </nav>
+    <div>
+      <h3>© 2024 | All Rights Reserved | Cauã Sales</h3>
+      <img :src="LogoFooter" alt="" />
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 30vh;
+  background-color: #12254d;
+  padding: 0 10vw;
+}
+
+a,
+h3 {
+  color: white;
+  list-style: none;
+  text-decoration: none;
+}
+
+a:hover {
+  color: rgba(127, 255, 212, 0.651);
+}
+
+li {
+  list-style: none;
+  font-size: 1.5em;
+  font-weight: 600;
+  padding: 10px 0;
+}
+
+img {
+  width: 10%;
+}
+
+div {
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  gap: 20px;
+  position: relative;
+  bottom: 50px;
+}
+</style>
